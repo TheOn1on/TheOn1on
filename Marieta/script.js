bea4ea9e28c9a5f53a2719c8f657f03af6d1693b
Georgi <theon11onringz@gmail.com>
2024-12-03 21:17:22 +0200

    Add files via upload

---
+++ Marieta/script.js
@@ -0,0 +1,11 @@
+document.getElementById('reveal-message').addEventListener('click', function() {
+    alert(' Обичам твоята доброта!');
+});
+
+document.getElementById('heart').addEventListener('click', function() {
+    alert('Сърцето ми бие само за теб!');
+});
+
+document.getElementById('bunny').addEventListener('click', function() {
+    alert('С твоето присъствие всичко става по-красиво и значимо!');
+});
